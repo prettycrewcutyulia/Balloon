3defe487cc8059feb256a989fc5628f7d2800cbe
prettycrewcutyulia <114954241+prettycrewcutyulia@users.noreply.github.com>
2023-11-17 00:54:38 +0300

    Initial Commit

---
+++ Balloon/BalloonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalloonApp.swift
+//  Balloon
+//
+//  Created by Юлия Гудошникова on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BalloonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
