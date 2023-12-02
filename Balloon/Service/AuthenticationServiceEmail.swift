013a8614fedf7798578b9727e3fce48f0fcbf963
prettycrewcutyulia <114954241+prettycrewcutyulia@users.noreply.github.com>
2023-12-02 23:49:02 +0300

    Update AuthenticationServiceEmail.swift

--- Balloon/Service/AuthenticationServiceEmail.swift
+++ Balloon/Service/AuthenticationServiceEmail.swift
@@ -9,20 +9,30 @@ import FirebaseAuth
 
 class AuthenticationServiceEmail {
     static let shared = AuthenticationServiceEmail()
+    
+    // гарантия, что у нас будет только один экземпляр во всем приложении
+    private init(){}
+    
+    private let auth = Auth.auth()
+    
+    var currentUser : User? {
+        return auth.currentUser
+    }
+    
     func signIn(email: String, password: String, completion: @escaping (Error?) -> Void) {
-        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+        auth.signIn(withEmail: email, password: password) { authResult, error in
             completion(error)
         }
     }
     
     func signUp(email: String, password: String, completion: @escaping (Error?) -> Void) {
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+        auth.createUser(withEmail: email, password: password) { authResult, error in
             completion(error)
         }
     }
     
     func resetPassword(email: String, completion: @escaping (Error?) -> Void) {
-        Auth.auth().sendPasswordReset (withEmail: email) { error in
+        auth.sendPasswordReset (withEmail: email) { error in
             completion(error)
         }
     }
